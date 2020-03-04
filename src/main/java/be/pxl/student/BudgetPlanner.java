4ffafd4e76deaadf28792bdfa5cf2c631213debd
seymaciftciPXL <seyma.ciftci@student.pxl.be>
2020-03-04 20:49:31 +0100

    logger oef gemaakt --> einde les 2

--- src/main/java/be/pxl/student/BudgetPlanner.java
+++ src/main/java/be/pxl/student/BudgetPlanner.java
@@ -1,6 +1,34 @@
 package be.pxl.student;
 
+import be.pxl.student.entity.Account;
+import be.pxl.student.util.BudgetPlannerException;
+import be.pxl.student.util.BudgetPlannerImporter;
+import be.pxl.student.util.BudgetPlannerMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.nio.file.Paths;
+import java.util.List;
+
 public class BudgetPlanner {
+    private static Logger logger = LogManager.getLogger( BudgetPlanner.class );
+
+    public static void main(String[] args) {
+        String csvFile = "src/main/resources/account_payments.csv";
+
+        try {
+            logger.info( "Starting import..." );
+            List<String> list =BudgetPlannerImporter.readCsvFile( Paths.get( csvFile ) );
+            logger.info( "Csv info import done" );
+            logger.info( "Starting account mapping" );
+            List <Account> accounts = new BudgetPlannerMapper().mapAccounts( list );
+            accounts.forEach( logger::debug );
+            logger.info( "account mapping done" );
+        } catch (BudgetPlannerException e) {
+            e.printStackTrace();
+            logger.error("exception importing accounts", e);
+        }
 
 
+    }
 }
